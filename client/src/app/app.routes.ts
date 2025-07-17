0280ca358f57916ec2578081900999d72db3ca2e
AbdelrhmanRabie3 <abdelrhmanrabie33@gmail.com>
2025-07-17 11:52:15 +0300

    Add navBar

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { NotfoundComponent } from "./notfound/notfound.component";
+import { HomeComponent } from "./home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", redirectTo: "/login", pathMatch: "full" },
+  {
+    path: "login",
+    loadComponent: () =>
+      import("./login/login.component").then((m) => m.LoginComponent),
+  },
+  {
+    path: "home",
+    loadComponent: () =>
+      import("./home/home.component").then((m) => m.HomeComponent),
+  },
+  {
+    path: "**",
+    loadComponent: () =>
+      import("./notfound/notfound.component").then((m) => m.NotfoundComponent),
+  },
+];
