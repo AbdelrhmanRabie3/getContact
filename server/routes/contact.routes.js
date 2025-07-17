b761c1f38171a7ee257c87b8e2946f20aa6a41ca
AbdelrhmanRabie3 <abdelrhmanrabie33@gmail.com>
2025-07-17 04:10:06 +0300

    Make only admin can add contacts

--- server/routes/contact.routes.js
+++ server/routes/contact.routes.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const contactController = require("../controllers/contact.controller");
-const {authenticateJWT,authorizeRoles} = require("../middlewares/auth.middleware");
+const {
+  authenticateJWT,
+  authorizeRoles,
+} = require("../middlewares/auth.middleware");
 
 router.use(authenticateJWT);
-router.post(
-  "/",
-  authorizeRoles("admin", "user"),
-  contactController.createContact
-);
+router.post("/", authorizeRoles("admin"), contactController.createContact);
 module.exports = router;
